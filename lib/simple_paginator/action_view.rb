9205b3528a490b2222a731105b8d8ae263e0061f
Nicolas Evans <me@nicolasevans.org>
2016-02-04 01:52:48 -0500

    more hackery

--- lib/simple_paginator/action_view.rb
+++ lib/simple_paginator/action_view.rb
@@ -1,14 +1,53 @@
 module SimplePaginator
   module ActionView
     def simple_paginator(collection, options = {})
-      options[:previous_label] ||= "<div>&#8592; Previous</div>"
-      options[:next_label]     ||= "<div>Next &#8594;</div>"
+      options = default_options.merge(options)
 
-      output = ""
-      output += options[:previous_label] if collection.previous_page_exists?
-      output += options[:next_label] if collection.next_page_exists?
+      output = outer_opening_tag(options)
+
+      output += inner_opening_tag(options)
+      output += previous_paginator_link(options, collection)
+      output += inner_closing_tag(options)
+
+      output += inner_opening_tag(options)
+      output += next_paginator_link(options, collection) # if collection.next_page_exists?
+      output += inner_closing_tag(options)
+
+      output += "</#{options[:pagination_outer_wrapper_node]}>"
 
       output.html_safe
+    end
+
+    private
+
+    def outer_opening_tag(options)
+      "<#{options[:pagination_outer_wrapper_node]} class=\"#{options[:pagination_outer_wrapper_class]}\">"
+    end
+
+    def inner_opening_tag(options)
+      "<#{options[:pagination_inner_wrapper_node]} class=\"#{options[:pagination_inner_wrapper_class]}\">"
+    end
+
+    def previous_paginator_link(options, collection)
+    end
+
+    def next_paginator_link(options, collection)
+    end
+
+    def inner_closing_tag(options)
+      "</#{options[:pagination_inner_wrapper_node]}>"
+    end
+
+    def default_options
+      {
+        pagination_outer_wrapper_node:           "ul",
+        pagination_outer_wrapper_class:          "pagination",
+        pagination_inner_wrapper_node:           "li",
+        pagination_inner_wrapper_class:          "",
+        pagination_inner_wrapper_disabled_class: "disabled",
+        previous_label:                          "&#8592; Previous",
+        next_label:                              "Next &#8594;",
+      }
     end
 
     ::ActionView::Base.send :include, self
